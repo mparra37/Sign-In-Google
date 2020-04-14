bd529e9cc422ffe038b48631087343ecd354ce8c
Mario Parra <marioparra375@gmail.com>
2020-04-13 17:52:55 -0700

    Google Sign In Example

---
+++ app/src/main/java/parra/mario/googlesigninexample/MainActivity.kt
@@ -0,0 +1,100 @@
+package parra.mario.googlesigninexample
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+const val RC_SIGN_IN = 343
+const val LOG_OUT = 234
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        sign_in_button.setSize(SignInButton.SIZE_WIDE)
+
+        sign_in_button.setOnClickListener{
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+        }
+
+        if(requestCode == LOG_OUT){
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                Toast.makeText(this,"Sesión terminada", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+
+            updateUI(account)
+        } catch (e: ApiException) {
+            Log.w("test_signin", "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(acct: GoogleSignInAccount?) {
+
+         if (acct != null){
+             val intent = Intent(this, PrincipalActivity::class.java)
+             intent.putExtra("id", acct.getId())
+             intent.putExtra("name", acct.getDisplayName())
+             intent.putExtra("email", acct.getEmail())
+             startActivityForResult(intent, LOG_OUT)
+         }
+    }
+
+
+}
---
+++ app/src/main/java/parra/mario/googlesigninexample/PrincipalActivity.kt
@@ -0,0 +1,34 @@
+package parra.mario.googlesigninexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import kotlinx.android.synthetic.main.activity_principal.*
+
+
+class PrincipalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+
+        val bundle = intent.extras
+
+        if (bundle != null){
+            val name = bundle.getString("name")
+            val email = bundle.getString("email")
+            tv_nombre.setText(name)
+            tv_email.setText(email)
+        }
+
+        btn_cerrar.setOnClickListener{
+            finish()
+        }
+
+    }
+
+    override fun onBackPressed() {
+
+    }
+
+}
